1dd7ceb5a0730704607c85065fbe5d29a44710c8
tamimhossen6238-cell <tamimhossen6238@gmail.com>
2025-12-27 23:42:40 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // এই লাইনটি সাদা স্ক্রিন দূর করবে
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -18,6 +19,12 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true
       }
     };
 });
+
